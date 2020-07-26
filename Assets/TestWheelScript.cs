2914dbebcd75a848138f2c20f23d48fcbc0adb3f
Maksim <maksalmak@gmail.com>
2020-07-27 00:24:15 +0300

    Worked self builded car

--- Assets/TestWheelScript.cs
+++ Assets/TestWheelScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TestWheelScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float force = 100;
     void Start()
     {
         
@@ -15,7 +15,7 @@ public class TestWheelScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * 10000);
+            GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * force);
         }
     }
 }
