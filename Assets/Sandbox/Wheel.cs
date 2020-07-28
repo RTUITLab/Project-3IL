96be16fae7a4a9a0965c8366258f25520caaa408
Maksim <maksalmak@gmail.com>
2020-07-28 03:40:34 +0300

    Done jumping car in sandbox

---
+++ Assets/Sandbox/Wheel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Wheel : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    public void DoBarrier(float force)
+    {
+        Debug.Log(name);
+        rigidBody.AddRelativeForce(Vector3.up * force);
+    }
+}
