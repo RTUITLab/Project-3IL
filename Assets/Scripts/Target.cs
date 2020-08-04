4f87b5fb56f7d1bd1b4bac9b887d969386a453c6
Dmitriy <Dimamakarov1941@yandex.ru>
2020-08-04 16:33:51 +0300

    Add normal sounds,music,voices
    in target script i add take damage voice,
    in prefabs i change only sounds
    also i make 2 scripts - music playes, enemy voice and add it in prefabs too

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,107 +2,92 @@
 using PathCreation;
 using UnityEngine;
 
-public class Target : MonoBehaviour
-{
-	[SerializeField] float _health = 100f;
-	[SerializeField] ParticleSystem _Explosion = null;
-	[SerializeField] ParticleSystem _Fire = null;
-	[SerializeField] AudioSource thisAudioSource = null;
-	[SerializeField] GameObject Body = null;
-	[SerializeField] PathFollower _pathFollower = null;
+public class Target : MonoBehaviour {
+    [SerializeField] float _health = 100f;
+    [SerializeField] ParticleSystem _Explosion = null;
+    [SerializeField] ParticleSystem _Fire = null;
+    [SerializeField] AudioSource thisAudioSource = null;
+    [SerializeField] GameObject Body = null;
+    [SerializeField] PathFollower _pathFollower = null;
     [SerializeField] PathCreator[] Creators = null;
     [SerializeField] float _delayBeforeStart = 0;
-	[SerializeField] float _speedBeforAttack = 5;
-	[SerializeField] float _normalSpeed = 1.5f;
-	public GameObject Player = null;
+    [SerializeField] float _speedBeforAttack = 5;
+    [SerializeField] float _normalSpeed = 1.5f;
+    public GameObject Player = null;
     public EnemyGunScript GunScript;
-	[SerializeField] float distance = 7;
-	[SerializeField] bool _NearPlayer = false;
+    [SerializeField] float distance = 7;
+    [SerializeField] bool _NearPlayer = false;
+    [SerializeField] VoiceEnemy _voiceEnemy = null;
 
-    private void Start()
-    {
-        int index = Random.Range(0, Creators.Length);
+    private void Start () {
+        int index = Random.Range (0, Creators.Length);
         _pathFollower.pathCreator = Creators[index];
-        StartCoroutine(Go());
-        Player = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine (Go ());
+        Player = GameObject.FindGameObjectWithTag ("Player");
         GunScript.Player = Player.transform;
     }
 
-	private void FixedUpdate ()
-    {
-        if (!_NearPlayer)
-        {
-            if (Vector3.Distance(Player.transform.position, gameObject.transform.position) > 200)
-            {
+    private void FixedUpdate () {
+        if (!_NearPlayer) {
+            if (Vector3.Distance (Player.transform.position, gameObject.transform.position) > 200) {
                 _pathFollower.speed = 8;
-            }
-            else if (Vector3.Distance(Player.transform.position, gameObject.transform.position) > 100)
-            {
+            } else if (Vector3.Distance (Player.transform.position, gameObject.transform.position) > 100) {
                 _pathFollower.speed = 6;
-            }
-            else if (Vector3.Distance(Player.transform.position, gameObject.transform.position) > distance)
-            {
+            } else if (Vector3.Distance (Player.transform.position, gameObject.transform.position) > distance) {
                 _pathFollower.speed = _speedBeforAttack;
-            }
-            else
-            {
-                StartCoroutine(Speed());
+            } else {
+                StartCoroutine (Speed ());
                 _pathFollower.speed = _normalSpeed;
                 _NearPlayer = true;
             }
         }
-	}
+    }
 
-    public void TakeDamage(float amount)
-    {
-        if (_health > 0)
-        {
+    public void TakeDamage (float amount) {
+        //with 15% chance, terrorist say something
+        if (Random.Range (0, 100) < 15)
+            _voiceEnemy.PlayVoice ();
+
+        if (_health > 0) {
             _health -= amount;
-            if (_health < 100 && !_Fire.gameObject.activeInHierarchy)
-            {
-                _Fire.gameObject.SetActive(true);
-            }
-            else if (_health <= 0f)
-            {
-                Die();
+            if (_health < 100 && !_Fire.gameObject.activeInHierarchy) {
+                _Fire.gameObject.SetActive (true);
+            } else if (_health <= 0f) {
+                Die ();
             }
         }
     }
 
-    void Die ()
-    {
+    void Die () {
         GunScript.enabled = false;
-        SpawnEnemies.instance.Spawn();
-		_Explosion.gameObject.SetActive (true);
-		_Fire.Stop ();
-		thisAudioSource.Play ();
-		Destroy (Body, 2f);
-		Destroy (gameObject, 2f);
-	}
+        SpawnEnemies.instance.Spawn ();
+        _Explosion.gameObject.SetActive (true);
+        _Fire.Stop ();
+        thisAudioSource.Play ();
+        Destroy (Body, 2f);
+        Destroy (gameObject, 2f);
+    }
 
-    IEnumerator Go()
-    {
-        yield return new WaitForSeconds(_delayBeforeStart);
+    IEnumerator Go () {
+        yield return new WaitForSeconds (_delayBeforeStart);
         _pathFollower.enabled = true;
     }
 
-    IEnumerator Speed()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+    IEnumerator Speed () {
+        while (true) {
+            yield return new WaitForSeconds (Random.Range (0.5f, 1.5f));
             _pathFollower.speed = 1.45f;
-            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+            yield return new WaitForSeconds (Random.Range (0.5f, 1.5f));
             _pathFollower.speed = 1.55f;
-            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+            yield return new WaitForSeconds (Random.Range (0.5f, 1.5f));
             _pathFollower.speed = 1.47f;
-            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+            yield return new WaitForSeconds (Random.Range (0.5f, 1.5f));
             _pathFollower.speed = 1.52f;
-            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+            yield return new WaitForSeconds (Random.Range (0.5f, 1.5f));
             _pathFollower.speed = 1.4f;
-            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+            yield return new WaitForSeconds (Random.Range (0.5f, 1.5f));
             _pathFollower.speed = 1.52f;
-            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+            yield return new WaitForSeconds (Random.Range (0.5f, 1.5f));
             _pathFollower.speed = 1.5f;
         }
     }
