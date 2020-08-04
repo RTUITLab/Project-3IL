335ccd69761aa90d710a9eb43d1ddf4a9a711a18
Danila <dakfadeev@gmail.com>
2020-08-04 12:41:06 +0300

    add restart button

---
+++ Assets/Scripts/VR_UI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VR_UI : MonoBehaviour
+{
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
