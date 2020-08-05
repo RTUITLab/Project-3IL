6246c267ab2dc57c4ba5e67c26ea825121ea67e1
Dmitriy <Dimamakarov1941@yandex.ru>
2020-08-05 13:20:16 +0300

    Fix AmmoBox, Add BulletHoleBruh

--- Assets/Scripts/HighlightSelection.cs
+++ Assets/Scripts/HighlightSelection.cs
@@ -9,41 +9,42 @@ public class HighlightSelection : MonoBehaviour
     Material DefaultMaterial = null;
     [SerializeField] KeyCode ReloadButton = KeyCode.R;
     [SerializeField] int AddAmmo = 60;
+    [SerializeField] Animator _topBoxAnimation=null;
     Transform Selection;
-    private Transform Camera;
 
-
-    private void Start()
-    {
-        Camera = gameObject.transform;
-    }
+    //old method
+    //wait, for what it?
+    //private Transform Camera;
+    //private void Start() => Camera = gameObject.transform;
 
     private void FixedUpdate()
     {
-        if (Selection != null)
+        if (Selection != null )
         {
-            var selectionRenderer = Selection.GetComponent<Renderer>();
-            selectionRenderer.material = DefaultMaterial;
-            Selection = null;
+        _topBoxAnimation.SetBool("TopBox",false);
+        //     var selectionRenderer = Selection.GetComponent<Renderer>();
+        //     selectionRenderer.material = DefaultMaterial;
+        //     Selection = null;
         }
-        Ray ray = new Ray(Camera.position, Camera.forward);
+
+        //old method
+        //Ray ray = new Ray(Camera.position, Camera.forward);
+        //new method
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
             Transform selection = hit.transform;
             if (selection.CompareTag("Selectable"))
             {
-                Renderer selectionRenderer = selection.GetComponent<Renderer>();
-                if (selectionRenderer != null)
+               //Renderer selectionRenderer = selection.GetComponent<Renderer>();
+                _topBoxAnimation.SetBool("TopBox",true);
+                if (Input.GetKey(ReloadButton))
                 {
-                    DefaultMaterial = selectionRenderer.material;
-                    selectionRenderer.material = HighlightMaterial;
-                    if (Input.GetKey(ReloadButton))
-                    {
-                        Gun.AmmoInPocket = AddAmmo;
-                        Gun.ReloadAmmoInfo();
-                    }
+                    Gun.AmmoInPocket = AddAmmo;
+                    Gun.ReloadAmmoInfo();
                 }
+
                 Selection = selection;
             }
         }
