f7d603b4c9701a9c1b917f457dddbe3f71a3f468
Shureck <Sashalev200149@gmail.com>
2020-08-07 18:03:07 +0300

    add script terrain

--- Assets/Scripts/SetTerrain.cs
+++ Assets/Scripts/SetTerrain.cs
@@ -3,33 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 //https://answers.unity.com/questions/391688/how-to-set-terrain-setheights-c.html
 public class SetTerrain : MonoBehaviour {
-    public Terrain terrain;
-    public TerrainData tData;
+    Terrain terr; // terrain to modify
+    int hmWidth; // heightmap width
+    int hmHeight; // heightmap height
 
-    public int xRes;
-    public int yRes;
-    public float[, ] heights;
-    //public Terrain myTerrain;
-    public int PlayerXi;
-    public int PlayerZi;
-    private GameObject Player;
+    int posXInTerrain; // position of the game object in terrain width (x axis)
+    int posYInTerrain; // position of the game object in terrain height (z axis)
+
+    int size = 50; // the diameter of terrain portion that will raise under the game object
+    float desiredHeight = 0; // the height we want that portion of terrain to be
+
     void Start () {
-        Player = GameObject.Find ("Camera");
-        tData = terrain.terrainData;
-        xRes = tData.heightmapResolution;
-        yRes = tData.heightmapResolution;
+
+        terr = Terrain.activeTerrain;
+        hmWidth = terr.terrainData.heightmapResolution;
+        hmHeight = terr.terrainData.heightmapResolution;
+
     }
 
     void Update () {
-        // we convert float of our position x in to int
-        PlayerXi = (int) Player.transform.position.x;
-        // we convert float of our position z in to int
-        PlayerZi = (int) Player.transform.position.z;
-        // we tell heights how big the map is
-        heights = tData.GetHeights (0, 0, xRes, yRes);
-        // we set z in to x AND x in to y
-        heights[PlayerZi, PlayerXi] = 0.01f;
-        tData.SetHeights (0, 0, heights);
+
+        // get the normalized position of this game object relative to the terrain
+        Vector3 tempCoord = (transform.position - terr.gameObject.transform.position);
+        Vector3 coord;
+        coord.x = tempCoord.x / terr.terrainData.size.x;
+        coord.y = tempCoord.y / terr.terrainData.size.y;
+        coord.z = tempCoord.z / terr.terrainData.size.z;
+
+        // get the position of the terrain heightmap where this game object is
+        posXInTerrain = (int) (coord.x * hmWidth);
+        posYInTerrain = (int) (coord.z * hmHeight);
+
+        // we set an offset so that all the raising terrain is under this game object
+        int offset = size / 2;
+
+        // get the heights of the terrain under this game object
+        float[, ] heights = terr.terrainData.GetHeights (posXInTerrain - offset, posYInTerrain - offset, size, size);
+
+        // we set each sample of the terrain in the size to the desired height
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                heights[i, j] = desiredHeight;
+
+        // go raising the terrain slowly
+        desiredHeight += Time.deltaTime;
+
+        // set the new height
+        terr.terrainData.SetHeights (posXInTerrain - offset, posYInTerrain - offset, heights);
+
     }
 
 }
