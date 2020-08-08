67dc846bdbe213e4375be2f6595105ca9249f859
Dmitriy <Dimamakarov1941@yandex.ru>
2020-08-08 11:14:38 +0300

    ammo ray work now! fine!
    problem was in tag, idk why, but defualt tag selectable changes everytine on player

--- Assets/Scripts/AmmoBox/AmmoRay.cs
+++ Assets/Scripts/AmmoBox/AmmoRay.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 
 public class AmmoRay : MonoBehaviour {
     [SerializeField] GunScript Gun = null;
+    [SerializeField] private Material HighlightMaterial = null;
+    Material DefaultMaterial = null;
     [SerializeField] KeyCode ReloadButton = KeyCode.R;
     [SerializeField] int AddAmmo = 60;
     [SerializeField] Animator _topBoxAnimation = null;
     Transform Selection;
-    [SerializeField] Transform gun;
+
+    //old method
+    //wait, for what it?
+    private Transform Camera;
+    private void Start () => Camera = gameObject.transform;
+
     private void FixedUpdate () {
         if (Selection != null) {
             _topBoxAnimation.SetBool ("TopBox", false);
+            //     var selectionRenderer = Selection.GetComponent<Renderer>();
+            //     selectionRenderer.material = DefaultMaterial;
             Selection = null;
         }
-        Ray ray = new Ray (gun.position, gun.forward);
+
+        //old method
+        Ray ray = new Ray (Camera.position, Camera.forward);
+        //new method
+        // var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
         RaycastHit hit;
-        Debug.DrawLine (gun.position, gun.forward, Color.green, 2000);
+        Debug.DrawRay (Camera.position, Camera.forward, Color.blue, 20);
         if (Physics.Raycast (ray, out hit)) {
             Transform selection = hit.transform;
-            if (selection.CompareTag ("Selectable")) {
+            Debug.Log (selection.tag);
+            if (selection.CompareTag ("Player")) {
+                //Renderer selectionRenderer = selection.GetComponent<Renderer>();
                 _topBoxAnimation.SetBool ("TopBox", true);
                 if (Input.GetKey (ReloadButton)) {
                     Gun.AmmoInPocket = AddAmmo;
                     Gun.ReloadAmmoInfo ();
                 }
+
                 Selection = selection;
             }
         }
