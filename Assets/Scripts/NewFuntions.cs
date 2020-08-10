174ab754e2c676e82d2ffe0a12b4d590b7d44e7c
Dmitriy <Dimamakarov1941@yandex.ru>
2020-08-10 11:06:12 +0300

    on start game on pause

--- Assets/Scripts/NewFuntions.cs
+++ Assets/Scripts/NewFuntions.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class NewFuntions : MonoBehaviour {
-    bool stop = false;
+    [SerializeField]
+    bool stop = true;
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start () {
+        if (stop)
+            Time.timeScale = 0;
+    }
     private void Update () {
         if (Input.GetKeyDown (KeyCode.Space) && stop == false) {
             Time.timeScale = 0;
