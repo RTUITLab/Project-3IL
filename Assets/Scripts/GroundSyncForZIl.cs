7f53cca14b1fa0edc0dacb7682b94e86988c9ff9
Dmitriy <Dimamakarov1941@yandex.ru>
2020-10-11 13:36:47 +0300

    🐛 FIX: sync enemy

---
+++ Assets/Scripts/GroundSyncForZIl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+public class GroundSyncForZIl : MonoBehaviour {
+
+    [SerializeField] GameObject way;
+    [SerializeField] float height = 1;
+
+    void FixedUpdate () {
+        RaycastHit hit = new RaycastHit ();
+        if (Physics.Raycast (transform.position, Vector3.down, out hit)) {
+            var distanceToGround = hit.distance;
+            print ($"12. TeleportOnGround -> distanceToGround : {distanceToGround}");
+            way.transform.DOMoveY (way.transform.position.y + height - 0.05f, 0);
+        } else
+            way.transform.DOMoveY (way.transform.position.y + 0.05f, 0);
+    }
+}
--- Assets/Scripts/TeleportOnGround.cs
+++ Assets/Scripts/TeleportOnGround.cs
@@ -1,24 +1,15 @@
-﻿using UnityEngine;
-
+﻿using DG.Tweening;
+using UnityEngine;
 public class TeleportOnGround : MonoBehaviour {
-	[SerializeField] GameObject Way;
+	[SerializeField] GameObject model;
 	[SerializeField] float height = 1;
-	[SerializeField][Range (1, 5)] float animSpeed = 1;
-
-	void Update () {
+	void FixedUpdate () {
 		RaycastHit hit = new RaycastHit ();
-		if (Physics.Raycast (transform.position, -Vector3.up, out hit)) {
+		if (Physics.Raycast (transform.position, Vector3.down, out hit)) {
 			var distanceToGround = hit.distance;
-			print ($"TeleportOnGround -> distanceToGround : {distanceToGround}");
-			if (distanceToGround > height + 0.1)
-				Way.transform.position = new Vector3 (Way.transform.position.x,
-					//  Model.transform.position.y - (distanceToGround - height)
-					Way.transform.position.y - 0.05f * animSpeed,
-					Way.transform.position.z);
-			else if (distanceToGround < height - 0.1)
-				Way.transform.position = new Vector3 (Way.transform.position.x,
-					Way.transform.position.y + 0.05f * animSpeed,
-					Way.transform.position.z);
-		}
+			print ($"12. TeleportOnGround -> distanceToGround : {distanceToGround}");
+			model.transform.DOMoveY (model.transform.position.y + height - distanceToGround, 0);
+		} else
+			model.transform.DOMoveY (model.transform.position.y + 0.05f, 0);
 	}
 }
