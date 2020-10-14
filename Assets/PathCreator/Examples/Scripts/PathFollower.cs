c0f268a9ddddb6bbe3d8105d7bbd7209f5d89c87
Danila <dakfadeev@gmail.com>
2020-10-14 13:30:49 +0300

    upd prefabs

--- Assets/PathCreator/Examples/Scripts/PathFollower.cs
+++ Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -4,47 +4,56 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 // Moves along a path at constant speed.
 // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
-public class PathFollower : MonoBehaviour {
+public class PathFollower : MonoBehaviour
+{
     public string PathName = "ZILpath";
     public PathCreator pathCreator;
     [SerializeField] EndOfPathInstruction endOfPathInstruction;
     [SerializeField] GameObject ural;
-	[SerializeField] bool isItPlayer=false;
+    [SerializeField] bool isItPlayer = false;
 
     public float speed = 5;
     float distanceTravelled;
     public bool needRotate;
     public bool findPathName = true;
     public int maxX = 1930;
-    private void Awake () {
+    private void Awake()
+    {
         if (findPathName)
-            pathCreator = GameObject.Find (PathName).GetComponent<PathCreator> ();
+            pathCreator = GameObject.Find(PathName).GetComponent<PathCreator>();
     }
 
-    private void start () {
+    private void start()
+    {
         if (pathCreator != null)
             // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
             pathCreator.pathUpdated += OnPathChanged;
     }
-    void Update () {
-        if (pathCreator != null) {
+    void Update()
+    {
+        if (pathCreator != null)
+        {
             distanceTravelled += speed * Time.deltaTime;
-            transform.position = pathCreator.path.GetPointAtDistance (distanceTravelled, endOfPathInstruction);
-            if (needRotate) {
-                var euler = pathCreator.path.GetRotationAtDistance (distanceTravelled, endOfPathInstruction).eulerAngles;
-                euler = new Vector3 (euler.x, euler.y, euler.z + 90);
-                transform.rotation = Quaternion.Euler (euler);
-            } else {
-                transform.rotation = pathCreator.path.GetRotationAtDistance (distanceTravelled, endOfPathInstruction);
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+            if (needRotate)
+            {
+                var euler = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction).eulerAngles;
+                euler = new Vector3(euler.x, euler.y, euler.z + 90);
+                transform.rotation = Quaternion.Euler(euler);
+            }
+            else
+            {
+                transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
             }
         }
-        if (transform.position.z > maxX&& isItPlayer) {
-            SceneManager.LoadScene ("Menu");
-			print ($"PathFollower -> LoadScene (\"Menu\");");
-            Destroy (ural);
+        if (transform.position.z > maxX && isItPlayer)
+        {
+            SceneManager.LoadScene("Menu");
+            print($"PathFollower -> LoadScene (\"Menu\");");
+            Destroy(ural);
         }
     }
     // If the path changes during the game, update the distance travelled so that the follower's position on the new path
     // is as close as possible to its position on the old path
-    void OnPathChanged () => distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath (transform.position);
+    void OnPathChanged() => distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
 }
