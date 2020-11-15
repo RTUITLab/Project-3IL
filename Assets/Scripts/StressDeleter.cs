65e4537c64d4f19bc309fd3ae08f47c86e2b4e9b
Dmitriy <Dimamakarov1941@yandex.ru>
2020-11-15 18:17:04 +0300

    feat: :sparkles: make clouds

---
+++ Assets/Scripts/StressDeleter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StressDeleter : MonoBehaviour {
+    [SerializeField] float timer = 2f;
+    Rigidbody rb;
+    bool stable = true;
+    private void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody> ();
+        rb.Sleep ();
+        StartCoroutine (unFreezeRotation (timer));
+    }
+    IEnumerator unFreezeRotation (float time) {
+        yield return new WaitForSeconds (time);
+        rb.velocity = new Vector3 (0, 0, 0);
+        rb.freezeRotation = false;
+        // StartCoroutine (offStable ());
+    }
+    IEnumerator offStable () {
+        yield return new WaitForSeconds (10f);
+    }
+    private void FixedUpdate () {
+        if (stable)
+            rb.velocity = new Vector3 (rb.velocity.x, rb.velocity.y, rb.velocity.z) / 2;
+    }
+}
