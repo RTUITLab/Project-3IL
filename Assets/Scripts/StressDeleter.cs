ff5300c0c78a15d7ec37c79644f2e6f843b44720
Dmitriy <Dimamakarov1941@yandex.ru>
2021-01-02 19:15:29 +0300

    fix: :ambulance: zil stability on start serpantin

--- Assets/Scripts/StressDeleter.cs
+++ Assets/Scripts/StressDeleter.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StressDeleter : MonoBehaviour {
+public class StressDeleter : MonoBehaviour
+{
     [SerializeField] float timer = 2f;
     Rigidbody rb;
     bool stable = true;
-    private void Start () {
-        rb = this.gameObject.GetComponent<Rigidbody> ();
-        rb.Sleep ();
-        StartCoroutine (unFreezeRotation (timer));
+    private void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        rb.Sleep();
+        StartCoroutine(unFreezeRotation(timer));
     }
-    IEnumerator unFreezeRotation (float time) {
-        yield return new WaitForSeconds (time);
-        rb.velocity = new Vector3 (0, 0, 0);
+    IEnumerator unFreezeRotation(float time)
+    {
+        yield return new WaitForSeconds(time);
         rb.freezeRotation = false;
-        // StartCoroutine (offStable ());
+        stable = false;
     }
-    IEnumerator offStable () {
-        yield return new WaitForSeconds (10f);
-    }
-    private void FixedUpdate () {
+    private void Update()
+    {
         if (stable)
-            rb.velocity = new Vector3 (rb.velocity.x, rb.velocity.y, rb.velocity.z) / 2;
+        {
+            rb.isKinematic = true;
+            rb.isKinematic = false;
+        }
     }
 }
